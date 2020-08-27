291a4cbdd6f0e56b1c9d12a3215a64a81bbdd36c
SILK Admin <admin@smartinfologiks.com>
2020-08-27 18:09:32 +0530

    First commit

---
+++ boot.php
@@ -0,0 +1,12 @@
+<?php
+require 'dompdf/vendor/autoload.php';
+
+require_once 'dompdf/lib/html5lib/Parser.php';
+require_once 'dompdf/lib/php-font-lib/src/FontLib/Autoloader.php';
+require_once 'dompdf/lib/php-svg-lib/src/autoload.php';
+require_once 'dompdf/src/Autoloader.php';
+Dompdf\Autoloader::register();
+
+?>
+
+
