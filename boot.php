6ac3658960e93e08eac62ca0429760e7ebf561f9
SILK Admin <admin@smartinfologiks.com>
2020-08-31 18:01:10 +0530

    Updated boot.php

--- boot.php
+++ boot.php
@@ -1,10 +1,10 @@
 <?php
-require 'dompdf/vendor/autoload.php';
+require __DIR__ .'/dompdf/vendor/autoload.php';
 
-require_once 'dompdf/lib/html5lib/Parser.php';
-require_once 'dompdf/lib/php-font-lib/src/FontLib/Autoloader.php';
-require_once 'dompdf/lib/php-svg-lib/src/autoload.php';
-require_once 'dompdf/src/Autoloader.php';
+require_once __DIR__ . '/dompdf/lib/html5lib/Parser.php';
+// require_once __DIR__ . '/dompdf/lib/php-font-lib/src/FontLib/Autoloader.php';
+// require_once __DIR__ .'/dompdf/lib/php-svg-lib/src/autoload.php';
+require_once __DIR__ .'/dompdf/src/Autoloader.php';
 Dompdf\Autoloader::register();
 
 ?>
