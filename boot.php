6fc628f791683ea6fe7aefc931515261f53405d7
SILK Admin <admin@smartinfologiks.com>
2021-10-26 19:59:15 +0530

    Updates to boot.php

--- boot.php
+++ boot.php
@@ -1,4 +1,6 @@
 <?php
+if(!defined('ROOT')) exit('No direct script access allowed');
+
 require __DIR__ .'/dompdf/vendor/autoload.php';
 
 require_once __DIR__ . '/dompdf/lib/html5lib/Parser.php';
@@ -7,6 +9,22 @@ require_once __DIR__ . '/dompdf/lib/html5lib/Parser.php';
 require_once __DIR__ .'/dompdf/src/Autoloader.php';
 Dompdf\Autoloader::register();
 
+use Dompdf\Dompdf;
+
+function printPDF($templateHTML, $params = []) {
+    // instantiate and use the dompdf class
+    $dompdf = new Dompdf();
+    $dompdf->loadHtml($templateHTML);
+    
+    // (Optional) Setup the paper size and orientation
+    $dompdf->setPaper('A4', 'landscape');
+    
+    // Render the HTML as PDF
+    $dompdf->render();
+    
+    // Output the generated PDF to Browser
+    $dompdf->stream();
+}
+
+
 ?>
-
-
